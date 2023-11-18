cd9321dea60aa951c2ea1357a8c6fdb8f28e897d
Lionnel Ngueukeu <nguetioof@gmail.com>
2023-11-18 10:11:23 +0100

    configuration des vues

--- Ecommerce.WebAssembly/Program.cs
+++ Ecommerce.WebAssembly/Program.cs
@@ -8,6 +8,8 @@ using Blazored.Toast;
 using Ecommerce.WebAssembly.Services.Contrats;
 using Ecommerce.WebAssembly.Services.Implementations;
 
+using CurrieTechnologies.Razor.SweetAlert2;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -24,5 +26,7 @@ builder.Services.AddScoped<IDashboardService,DashboardService>();
 builder.Services.AddScoped<IPanierService, PanierService>();
 builder.Services.AddScoped<IProduitService, ProduitService>();
 builder.Services.AddScoped<IVenteService, VenteService>();
+
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
